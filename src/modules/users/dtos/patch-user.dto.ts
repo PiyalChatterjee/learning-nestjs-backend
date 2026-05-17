53347b5218d2721248867c324e63a2d6e898388b
Piyal Chatterjee <piyal.chatterjee@neudesic.com>
2026-05-18 02:33:31 +0530

    chore: update DTOs to simplify structure and add Compodoc dependency
    - Added @compodoc/compodoc dependency to package.json.
    - Refactored PatchPostDto to extend CreatePostDto without additional properties.
    - Simplified PatchUserDto to extend UpdateUserDto without additional properties.
    - Updated UpdateUserDto to extend CreateUserDto while omitting the password field.

--- src/modules/users/dtos/patch-user.dto.ts
+++ src/modules/users/dtos/patch-user.dto.ts
@@ -1,23 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { UpdateUserDto } from './update-user.dto';
 
-export class PatchUserDto extends PartialType(UpdateUserDto) {
-  @ApiPropertyOptional({
-    description: 'First name of the user',
-    example: 'John',
-  })
-  firstName?: string;
-
-  @ApiPropertyOptional({
-    description: 'Last name of the user',
-    example: 'Doe',
-  })
-  lastName?: string;
-
-  @ApiPropertyOptional({
-    description: 'Email address of the user',
-    example: 'john.doe@example.com',
-  })
-  email?: string;
-}
+export class PatchUserDto extends PartialType(UpdateUserDto) {}
--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -1,23 +1,6 @@
-import { OmitType } from '@nestjs/mapped-types';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional, OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends OmitType(CreateUserDto, ['password'] as const) {
-  @ApiProperty({
-    description: 'First name of the user',
-    example: 'John',
-  })
-  firstName: string;
-
-  @ApiPropertyOptional({
-    description: 'Last name of the user',
-    example: 'Doe',
-  })
-  lastName: string;
-
-  @ApiProperty({
-    description: 'Email address of the user',
-    example: 'john.doe@example.com',
-  })
-  email: string;
-}
+export class UpdateUserDto extends OmitType(CreateUserDto, [
+  'password',
+] as const) {}
